12e9268c6c17ba16fb276dc7c2abeaa190854003
saysky2 <10852akash@gmail.com>
2024-09-22 20:59:08 +0530

    d2

---
+++ 02/signupform.js
@@ -0,0 +1,22 @@
+const uid = "saysky2";
+
+var fullName = "Akash Gupta";
+var emailId = "ansugupta@gmail.com";
+var password = "akashgupta";
+var confirmPassword = "akashgupta";
+var courseCount = 0;
+var isloggedInzFromGoogle = false;
+
+
+// fullName = prompt("Please enter your full name: ");
+
+console.log(uid);
+console.log("Your full name: " + fullName);
+console.log(emailId)
+
+console.log(`
+    with uid is ${uid};
+    Course Count: ${courseCount};
+    Password: ${password.length} characters long.
+    Confirm Password: ${confirmPassword.length} characters long.
+`);
---
+++ 02/variable.js
@@ -0,0 +1,17 @@
+var fullName = "saysky2";
+
+var isLoggedIn = false;
+
+var countLoggedIn = 50;
+
+// console.log(fullName);
+// console.log("fullName");
+// console.log(countLoggedIn);
+
+var paymentMode;
+
+// console.log(paymentMode);
+
+paymentMode = "Cerdit Card";
+
+console.log(paymentMode);
